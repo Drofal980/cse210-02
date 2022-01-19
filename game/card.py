78625d36855cebc0bbdd6046f823f0f5c0bd873d
Brandon Van Blarcum <41463636+Drofal980@users.noreply.github.com>
2022-01-19 10:39:51 -0700

    starting files

---
+++ game/card.py
@@ -0,0 +1,15 @@
+class Card():
+    """A paper card with a numerical value
+   
+    Attributes:
+        value (int): The number on a card.
+        points (int): The number of points the card is worth.
+    """
+
+# 2) Create the class constructor. Use the following method comment.
+def __init__(self):
+        """Constructs a new instance of Card with a value and points attribute.
+
+        Args:
+            self (Card): An instance of Card.
+        """
---
+++ game/director.py
@@ -0,0 +1,66 @@
+from game.cards import Card
+
+
+class Director:
+    """A person who directs the game. 
+    
+    The responsibility of a Director is to control the sequence of play.
+
+    Attributes:
+        dice (List[Die]): A list of Die instances.
+        is_playing (boolean): Whether or not the game is being played.
+        score (int): The score for one round of play.
+        total_score (int): The score for the entire game.
+    """
+
+    def __init__(self):
+        """Constructs a new Director.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        self.is_playing = True
+        self.score = 300
+        self.total_score = 0
+
+
+    def start_game(self):
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.is_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+    def get_inputs(self):
+        """Ask the user if they want to roll.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        
+       
+    def do_updates(self):
+        """Updates the player's score.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        if not self.is_playing:
+            return 
+
+        
+
+    def do_outputs(self):
+        """Displays the card and the score. Also asks the player if the next card will be higher or lower. 
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        if not self.is_playing:
+            return
+        
+        
